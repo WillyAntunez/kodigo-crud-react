457de9252499e650bdd6cc74ca7d1c3eac061d84
Willy Antunez <willyantunezg@gmail.com>
2023-03-03 19:54:34 -0600

    [fix] NavBar: Cambie Link por NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
 
         <nav className="navbar navbar-expand navbar-light bg-light">
-            <Link className="navbar-brand" to='/'>CrudApp</Link>
+            <NavLink className="navbar-brand" to='/'>CrudApp</NavLink>
 
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to='videogames'>Videojuegos</Link>
+                        <NavLink className="nav-link" activeClassName="active" to='videogames'>Videojuegos</NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to='cellphones'>Celulares</Link>
+                        <NavLink className="nav-link" to='cellphones'>Celulares</NavLink>
                     </li>
                 </ul>
             </div>
